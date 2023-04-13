7f293f84360de8c42ff3f671b233daf8ad3966b9
nestaenow <nestaenow@gmail.com>
2023-04-13 13:12:39 +0100

    Setup & init testing

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hey Nesta!</Text>
+      <View style={styles.body}>
+        <Text>Welcome to the React Native World!</Text>
+        <Text>Enjoy and Let Your Creativity Speak</Text>
+      </View>
+      <Button title="Let's Go" />
     </View>
   );
 }
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  title: {
+    color: "#f00",
+    fontWeight: 700,
+    fontSize: 20,
+    marginBottom: 40,
+  },
+  body: { marginBottom: 40 },
 });
